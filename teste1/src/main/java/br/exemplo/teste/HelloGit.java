477c83a84597c767a8eb42bf681cc44ef15ae1d3
Helton <heltonuema@gmail.com>
2016-11-24 03:12:43 -0200

    msg despedida task 6

--- teste1/src/main/java/br/exemplo/teste/HelloGit.java
+++ teste1/src/main/java/br/exemplo/teste/HelloGit.java
@@ -5,6 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 
 		System.out.println("Olá Git 3! - Hello Git 3!");
-		System.out.println("Tchau Git 3! - ");
+		System.out.println("Tchau Git 3! - Bye Git 3!");
 	}
 }
