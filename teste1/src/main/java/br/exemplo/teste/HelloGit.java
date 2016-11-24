596e80657a683589426a490e4605109beda41ec6
Helton <heltonuema@gmail.com>
2016-11-24 03:14:40 -0200

    resolucao conflito task 6

--- teste1/src/main/java/br/exemplo/teste/HelloGit.java
+++ teste1/src/main/java/br/exemplo/teste/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 
-		System.out.println("Ola Git 3! - Hello Git 3!");
+		System.out.println("Olá Git 3! - Hello Git 3!");
+		System.out.println("Tchau Git 3! - ");
 	}
 }
